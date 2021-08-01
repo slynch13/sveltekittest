75aec1e175770fc1dd5106eae8a1b1a48b2afbba
Sean Lynch <slynch13@gmail.com>
2021-08-01 03:33:02 +0000

    Moved to base

---
+++ src/lib/header.svelte
@@ -0,0 +1,7 @@
+<header class="bg-indigo-600">
+    <nav class="text-white p-8">
+        <a href="/" sveltekit:prefetch>Home</a>
+        <a href="/about" sveltekit:prefetch>About</a>
+        <a href="/page2" sveltekit:prefetch>Page 2</a>
+    </nav>
+</header>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Header from '$lib/header.svelte'
+    import "../styles/tailwind-output.css"
+</script>
+<div >
+    <Header></Header>
+    <main>
+        <slot></slot>
+    </main>
+</div>
+
+
